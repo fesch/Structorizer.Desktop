e2b0401423c80ed0887d3c0cf74f9f0170b98eb3
Kay Gürtzig <kay.guertzig@fh-erfurt.de>
2016-01-14 22:39:41 +0100

    Anticipated version number 3.23-14 prepared

--- src/lu/fisch/structorizer/elements/Element.java
+++ src/lu/fisch/structorizer/elements/Element.java
@@ -105,7 +105,7 @@ import javax.swing.ImageIcon;
 
 public abstract class Element {
 	// Program CONSTANTS
-	public static String E_VERSION = "3.23-14dev";
+	public static String E_VERSION = "3.23-14";
 	public static String E_THANKS =
 	"Developed and maintained by\n"+
 	" - Robert Fisch <robert.fisch@education.lu>\n"+
