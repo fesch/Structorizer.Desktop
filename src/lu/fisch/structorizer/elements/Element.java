b74e6e1626d0fc2ff01e3e6d3bf051568b9270cc
ale <ale@mbpdialessandro.homenet.telecomitalia.it>
2021-03-25 21:05:47 +0100

    ARM Translator

--- src/lu/fisch/structorizer/elements/Element.java
+++ src/lu/fisch/structorizer/elements/Element.java
@@ -250,6 +250,9 @@ import javax.swing.ImageIcon;
  * @author Bob Fisch
  */
 public abstract class Element {
+
+        public static boolean ARM_VISUAL=false;
+	public static boolean E_VARIABLE_ARM=false;
 	
 	/** This enumeration type distinguishes drawing contexts for selection display */
 	// START KGU#906 2021-01-06: Enh. #905 need another context to suppress triangles
